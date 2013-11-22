99fad7574e3c4a9b8a889319455b983260c1f06e
Satoru Majima <neo.otokomae@gmail.com>
2013-11-22 14:21:02 +0900

    RSSからの情報取得機能の作成 refs #4
    get($url) にて URLから RSS情報を自身に格納する機能
    build($xml) にて、XMLテキストから RSS情報を自身に格納する機能
    
    新規登録時は URL からもろもろデータを取得。
    登録後は RSSデータ上に xml をテキストで保存するので
    その xml 内容からデータを取得する。

--- Model/RssLoader.php
+++ Model/RssLoader.php
@@ -1,4 +1,6 @@
 <?php
+App::uses('HttpSocket', 'Network/Http');
+
 /**
  * RssLoader
  *
@@ -6,5 +8,77 @@
  */
 class RssLoader
 {
+	protected $http;
 
+	/**
+	 * 文字コード
+	 * get() で URLから取得したときのみそのエンコーディングがセットされる
+	 */
+	public $encoding;
+
+	/** RSSの channel */
+	public $channel;
+
+	/** RSS の items */
+	public $items;
+
+	/**
+	 * コンストラクタ
+	 */
+	public function __construct()
+	{
+		$this->http = new HttpSocket();
+	}
+
+	/**
+	 * Xmlを読み込んでデータの取得を行う
+	 *
+	 * @param   String  $input  URL, XML文字列（Xmlクラスの buildを同じ動き）
+	 */
+	public function get($url)
+	{
+		try {
+			$response = $this->http->get($url);
+
+			if (!$response->isOk()) {
+				return false;
+			}
+
+			// 文字コードの解析
+			preg_match('/.*charset=(.+)/', $response->headers['Content-Type'], $matches);
+			$this->encoding = !empty($matches[1]) ? $matches[1] : null;
+
+			$this->build($response->body);
+
+		} catch (SocketException $e) {
+			return false;
+		} catch (XmlException $e) {
+			return false;
+		}
+
+		return $this;
+	}
+
+	/**
+	 * Xmlを読み込んでデータの取得を行う
+	 *
+	 * @param   String  $input  URL, XML文字列（Xmlクラスの buildを同じ動き）
+	 * @param   Int     $limit  XMLから取得するデータ数
+	 */
+	public function build($input)
+	{
+		try {
+			$xml = Xml::build($input);
+
+			$this->channel = $xml->channel;
+			$this->items   = $xml->channel->item;
+
+			echo $this->items->count();
+
+		} catch (XmlException $e) {
+			return false;
+		}
+
+		return $this;
+	}
 }
